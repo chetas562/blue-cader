7f5cf624cf83a76331b605b720baa9a9249d1b76
chetas562 <70834532+chetas562@users.noreply.github.com>
2021-01-06 12:49:19 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# blue-cader
-watch
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,13 @@
+var Hour;
+var Minute;
+var Seconds
+function setup() {
+  createCanvas(800,400);
+  createSprite(400, 200, 50, 50);
+
+}
+
+function draw() {
+  background("black");
+  drawSprites();
+}
